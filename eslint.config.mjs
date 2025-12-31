672fd80409c2249fb067bdd542bfb94b186ed80c
Tegar <07mtegar@gmail.com>
2025-12-31 16:23:49 +0800

    fix bugs host value

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,8 +8,18 @@ export default defineConfig([
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      sourceType: 'commonjs',
+    },
+  },
   daStyle,
 ]);
